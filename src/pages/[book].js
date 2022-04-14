5d11a328b37be0dec20c9aa70a19c3758446a701
Steven Bister <stevebister@gmail.com>
2022-04-14 18:44:43 +0100

    Feat: Adds query for books within the same series

--- src/pages/[book].js
+++ src/pages/[book].js
@@ -1,8 +1,9 @@
 import getSanityContent from '@lib/sanity'
+import Link from 'next/link'
 import Image from 'next/image'
 import {PortableText} from '@portabletext/react'
 
-export default function Book ({title, author, series, blurb, cover, readStatus}) {
+export default function Book ({title, author, series, blurb, cover, readStatus, related}) {
   return (
     <>
       {title ? <h1>{title}</h1> : null}
@@ -10,15 +11,34 @@ export default function Book ({title, author, series, blurb, cover, readStatus})
       {author ? <p>{author}</p> : null}
       {readStatus ? <p>{readStatus}</p> : null}
 
-      {cover ? <Image src={cover.url} alt={cover.altText} width="320" height="500" layout='intrinsic' /> : null}
+      {cover ? <Image src={cover.url} alt={cover.altText} width="320" height="500" /> : null}
 
       {blurb ? <PortableText value={blurb} /> : null}
+
+      {related ?
+        <>
+          <h2>More books in this series...</h2>
+          <ul>
+            {related.map(({_id, title, slug, cover}) => (
+              <li key={_id}>
+                <Link href={`/${slug.current}`}>
+                  <a>
+                    <Image src={cover.asset.url} alt={cover.asset.altText} width="160" height="250" />
+                    <h3>{title}</h3>
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </>
+      : null}
     </>
   )
 }
 
 export async function getStaticProps({ params }) {
-  const data = await getSanityContent({
+  // Get current page book
+  const BookBySlug = await getSanityContent({
     query: `
       query BookBySlug($slug: String!) {
         allBook(where: { slug: { current: { eq: $slug } } }) {
@@ -45,12 +65,38 @@ export async function getStaticProps({ params }) {
     },
   })
 
-  const [book] = data.allBook
+  const [book] = BookBySlug.allBook
 
   const {title, blurbRaw: blurb, readStatus} = book
   const author = book.author ? book.author.name : null
   const series = book.series ? book.series.name : null
   const cover = book.cover ? book.cover.asset : null
+
+  // Get related books
+  const BooksBySeries = await getSanityContent({
+    query: `
+      query BooksBySeries($series: String!) {
+        allBook(where: { series: { name: { eq: $series } } }) {
+          _id
+          title
+          slug {
+            current
+          }
+          cover {
+            asset {
+              url
+              altText
+            }
+          }
+        }
+      }
+    `,
+    variables: {
+      series
+    }
+  })
+
+  const related = BooksBySeries ? BooksBySeries.allBook : null
 
   return {
     props: {
@@ -60,6 +106,7 @@ export async function getStaticProps({ params }) {
       blurb,
       cover,
       readStatus,
+      related
     }
   }
 }
