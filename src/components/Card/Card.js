4829d898ab4b9c1b8119137c7629c7c8991dcb7c
Steven Bister <stevebister@gmail.com>
2022-04-22 11:46:16 +0100

    Feat: Adjusts card sizes and spacing in the grid

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,17 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import CardImage from './CardImage';
 
 const Card = ({ title, slug, cover }) => {
   return (
-    <div className="card">
-      <CardImage image={cover} />
+    <Link href={`/${slug.current}`}>
+      <a className="color-dark">
+        <div className="card">
+          <CardImage image={cover} />
 
-      <h3 className="card__heading fs-1">
-        <Link href={`/${slug.current}`}>
-          <a className="color-dark stretch-link">{title}</a>
-        </Link>
-      </h3>
-    </div>
+          <h3 className="sr-only card__heading fs-1">{title}</h3>
+        </div>
+      </a>
+    </Link>
   );
 };
 
