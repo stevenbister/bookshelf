4b669a2656b83df6ef2da8b817a22504d1976419
Steven Bister <stevebister@gmail.com>
2022-04-22 15:46:28 +0100

    Feat: Add a field to our book object containing an array of related books

--- src/pages/index.js
+++ src/pages/index.js
@@ -45,38 +45,79 @@ export async function getStaticProps() {
     `,
   });
 
-  const books = data.allBook
-    .map((book) => {
-      const cover = book.cover ? book.cover : null;
-      const series = book.series ? book.series.name : null;
-
-      return {
-        _id: book._id,
-        title: book.title,
-        author: book.author.name,
-        slug: book.slug,
-        series,
-        bookNumber: book.bookNumber,
-        cover,
-      };
-    })
-    .sort((a, b) => {
-      // Sort author alphabetically
-      // If the first item comes first in the alphabet, move it up
-      // Otherwise move it down
-      if (a.author > b.author) return 1;
-      if (a.author < b.author) return -1;
-
-      // The sort by the series
-      if (a.series > b.series) return 1;
-      if (a.series < b.series) return -1;
-
-      // Finally order the series by book number
-      if (a.bookNumber > b.bookNumber) return 1;
-      if (a.bookNumber < b.bookNumber) return -1;
+  /**
+   * Query all of our books by the series
+   *
+   * @param {String} variable Title of the series we want to filter our query by
+   * @returns {Promise} A Promise containing an object of our query result
+   */
+  const queryBooksBySeries = async (variable) => {
+    const BooksBySeries = await getSanityContent({
+      query: `
+          query BooksBySeries($series: String!) {
+            allBook(
+              where: { series: { name: { eq: $series } } },
+              sort: { bookNumber: ASC }
+            ) {
+              _id
+              title
+              slug {
+                current
+              }
+              cover {
+                asset {
+                  url
+                  altText
+                }
+              }
+            }
+          }
+        `,
+      variables: {
+        series: variable,
+      },
     });
 
-  const total = books.length;
+    return BooksBySeries;
+  };
+
+  const books = await Promise.all(
+    data.allBook
+      .filter((book) => book.bookNumber === 1 || book.bookNumber === null)
+      .map(async (book) => {
+        const cover = book.cover ? book.cover : null;
+        const series = book.series ? book.series.name : null;
+
+        // Get books by series and pass them into each response
+        const querySeries = await queryBooksBySeries(series);
+        const relatedBooks = querySeries ? querySeries.allBook : null;
+
+        return {
+          _id: book._id,
+          title: book.title,
+          author: book.author.name,
+          slug: book.slug,
+          series,
+          bookNumber: book.bookNumber,
+          cover,
+          relatedBooks,
+        };
+      }),
+  );
+
+  books.sort((a, b) => {
+    // Sort author alphabetically
+    // If the first item comes first in the alphabet, move it up
+    // Otherwise move it down
+    if (a.author > b.author) return 1;
+    if (a.author < b.author) return -1;
+
+    // Then sort by title
+    if (a.title > b.title) return 1;
+    if (a.title < b.title) return -1;
+  });
+
+  const total = data.allBook.length;
 
   return {
     props: { books, total },
