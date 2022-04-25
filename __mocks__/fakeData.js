754688f7a7db1f9be6ef739e3a0e40a46bbae126
Steven Bister <stevebister@gmail.com>
2022-04-25 12:52:14 +0100

    Test: Updates fake data to match new live data

--- __mocks__/fakeData.js
+++ __mocks__/fakeData.js
@@ -1,31 +1,55 @@
 const fakeData = {
   allBook: [
     {
-      _id: '5483de4a-44fb-4b1c-8f13-586b0aa0e91b',
-      title: "'Salems Lot",
-      slug: {
-        current: 'salems-lot',
-      },
+      _id: 'f4e7685b-1119-411f-855b-6f77e4816f6b',
+      title: 'The Eye of the World',
       author: {
-        name: 'Stephen King',
+        name: 'Robert Jordan',
       },
-      series: null,
-      blurbRaw: null,
-      cover: null,
-      readStatus: ['Read'],
-    },
-    {
-      _id: 'eb00b46d-45ef-456c-91c6-14d367cbe611',
-      title: 'The Great Hunt',
       slug: {
-        current: 'the-great-hunt',
-      },
-      author: {
-        name: 'Robert Jorden',
+        current: 'the-eye-of-the-world',
       },
       series: {
         name: 'The Wheel of Time',
       },
+      bookNumber: 1,
+      blurbRaw: [
+        {
+          _key: '7dbedc86f6f4',
+          _type: 'block',
+          children: [
+            {
+              _key: 'adc9fcaa8f04',
+              _type: 'span',
+              marks: [],
+              text: 'When she arrives in a small village in the Two Rivers, Moiraine discovers three young men, each of whom might be the long-awaited and reviled Chosen One, the Dragon Reborn. But she is not the only stranger new to the village, nor the only one searching.\n\nIn a race against time and the agents of the Shadow, she must guide her charges through lands of myth and legend, toward allies both new and old, and into the footsteps of prophecy.\n\nThe Wheel of Time turns, and an epic adventure begins.',
+            },
+          ],
+          markDefs: [],
+          style: 'normal',
+        },
+      ],
+      readStatus: ['Read'],
+      cover: {
+        asset: {
+          url: 'https://cdn.sanity.io/images/vvmrkra8/production/3a0831dc96a22f3ae304e80e44e76e05e0bcb5c8-319x500.jpg',
+          altText: null,
+        },
+      },
+    },
+    {
+      _id: 'eb00b46d-45ef-456c-91c6-14d367cbe611',
+      title: 'The Great Hunt',
+      author: {
+        name: 'Robert Jordan',
+      },
+      slug: {
+        current: 'the-great-hunt',
+      },
+      series: {
+        name: 'The Wheel of Time',
+      },
+      bookNumber: 2,
       blurbRaw: [
         {
           _key: '9dcaf55300d6',
@@ -56,49 +80,48 @@ const fakeData = {
           style: 'normal',
         },
       ],
+      readStatus: ['Read'],
       cover: {
         asset: {
           url: 'https://cdn.sanity.io/images/vvmrkra8/production/a1edf48182cb87b4216d07c05aac08b7ffa9e8d6-636x1000.jpg',
-          altText: '',
+          altText: null,
         },
       },
-      readStatus: ['Read'],
     },
     {
-      _id: 'f4e7685b-1119-411f-855b-6f77e4816f6b',
-      title: 'The Eye of the World',
-      slug: {
-        current: 'the-eye-of-the-world',
-      },
+      _id: '5483de4a-44fb-4b1c-8f13-586b0aa0e91b',
+      title: "'Salems Lot",
       author: {
-        name: 'Robert Jorden',
+        name: 'Stephen King',
       },
-      series: {
-        name: 'The Wheel of Time',
+      slug: {
+        current: 'salems-lot',
       },
+      series: null,
+      bookNumber: null,
       blurbRaw: [
         {
-          _key: '7dbedc86f6f4',
+          _key: '33ed87a279c8',
           _type: 'block',
           children: [
             {
-              _key: 'adc9fcaa8f04',
+              _key: '54713c3954ae',
               _type: 'span',
               marks: [],
-              text: 'When she arrives in a small village in the Two Rivers, Moiraine discovers three young men, each of whom might be the long-awaited and reviled Chosen One, the Dragon Reborn. But she is not the only stranger new to the village, nor the only one searching.\n\nIn a race against time and the agents of the Shadow, she must guide her charges through lands of myth and legend, toward allies both new and old, and into the footsteps of prophecy.\n\nThe Wheel of Time turns, and an epic adventure begins.',
+              text: "Thousands of miles away from the small township of 'Salem's Lot, two terrified people, a man and a boy, still share the secrets of those clapboard houses and tree-lined streets. They must return to 'Salem's Lot for a final confrontation with the unspeakable evil that lives on in the town.",
             },
           ],
           markDefs: [],
           style: 'normal',
         },
       ],
+      readStatus: ['Read'],
       cover: {
         asset: {
-          url: 'https://cdn.sanity.io/images/vvmrkra8/production/3a0831dc96a22f3ae304e80e44e76e05e0bcb5c8-319x500.jpg',
-          altText: '',
+          url: 'https://cdn.sanity.io/images/vvmrkra8/production/18338ea6302b1f48ab2e9895ed2b310bb7f86214-1524x2339.jpg',
+          altText: null,
         },
       },
-      readStatus: ['Read'],
     },
   ],
 };
