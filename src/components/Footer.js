1849c14344cd79874f51e96e850f0fc4e690e4e6
Steven Bister <stevebister@gmail.com>
2022-04-26 15:29:11 +0100

    Feat: Adds gh link and styles footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <footer className="footer"></footer>;
+  return (
+    <footer className="footer">
+      <p></p>
+      <a href="https://github.com/stevenbister/bookshelf">GitHub</a>
+    </footer>
+  );
 };
 
 export default Footer;
