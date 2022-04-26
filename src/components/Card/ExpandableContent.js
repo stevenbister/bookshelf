3040be8210677948bc92c92c496a2aa7665d66ec
Steven Bister <stevebister@gmail.com>
2022-04-26 16:30:55 +0100

    Feat: Add height animation to the reveal content
    Because we are animating the height we can't use row-gap anymore as
    the gaps still get generated. Instead using a bottom margin on the child

--- src/components/Card/ExpandableContent.js
+++ src/components/Card/ExpandableContent.js
@@ -4,8 +4,11 @@ const ExpandableContent = ({ id, series, openSeries, relatedBooks }) => {
   return (
     <li
       id={id}
-      className="grid__item--full-width"
-      hidden={series === openSeries ? false : true}
+      className={
+        series === openSeries
+          ? 'grid__item--full-width'
+          : 'grid__item--hidden grid__item--full-width'
+      }
     >
       <h2 className="fs-3" style={{ marginBottom: 'var(--space-2xs)' }}>
         {series}
