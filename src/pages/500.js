0793756d6d766fd0910f5782381c17753d9b11af
Steven Bister <stevebister@gmail.com>
2022-04-27 13:53:41 +0100

    Feat: Adds error pages

---
+++ src/pages/500.js
@@ -0,0 +1,16 @@
+const Custom404 = () => {
+  return (
+    <div
+      style={{
+        height: '50vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+      }}
+    >
+      <h1 style={{ textAlign: 'center' }}>500 - Server-side error occurred</h1>
+    </div>
+  );
+};
+
+export default Custom404;
