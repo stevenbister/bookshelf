5ded9e499ac59c508f68749163844bf9718ae40b
Steven Bister <stevebister@gmail.com>
2022-04-27 16:56:18 +0100

    Chore: Remove console.log

--- src/components/Card/CardImage.js
+++ src/components/Card/CardImage.js
@@ -1,30 +1,27 @@
 import Image from 'next/image';
 import placeholder from '../../../public/img/Placeholder.png';
 
-const CardImage = ({ image }) => {
-  console.log(image);
-  return (
-    <div className="card__image">
-      {image ? (
-        <Image
-          src={image.asset.url}
-          alt={image.asset.altText ? image.asset.altText : ''}
-          width="160"
-          height="250"
-          layout="responsive"
-          placeholder="blur"
-          blurDataURL={image.asset.metadata.lqip}
-        />
-      ) : (
-        <Image
-          src={placeholder}
-          alt=""
-          width="160"
-          height="250"
-          layout="responsive"
-        />
-      )}
-    </div>
-  );
-};
+const CardImage = ({ image }) => (
+  <div className="card__image">
+    {image ? (
+      <Image
+        src={image.asset.url}
+        alt={image.asset.altText ? image.asset.altText : ''}
+        width="160"
+        height="250"
+        layout="responsive"
+        placeholder="blur"
+        blurDataURL={image.asset.metadata.lqip}
+      />
+    ) : (
+      <Image
+        src={placeholder}
+        alt=""
+        width="160"
+        height="250"
+        layout="responsive"
+      />
+    )}
+  </div>
+);
 export default CardImage;
