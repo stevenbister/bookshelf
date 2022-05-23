3883690c2c84cda53ffeb898b92bbd68d14ffb44
Steven Bister <stevebister@gmail.com>
2022-05-23 17:05:52 +0100

    Feat: Add new function to handle sorting
    New sortBooks() function lets us sort our books array on the front end

--- src/lib/queryBooks.js
+++ src/lib/queryBooks.js
@@ -130,8 +130,13 @@ const queryBooksBySeries = async (series) => {
 };
 
 /**
+ * Build a new array filtering and mapping the books array so we can nest related books within
+ * each item.
  *
  * @param {Array} allBooks
+ * @param {String} sortBy
+ *
+ * @returns {Array} New array of books
  */
 const buildBooksArray = async (allBooks) => {
   const books = await Promise.all(
@@ -139,7 +144,7 @@ const buildBooksArray = async (allBooks) => {
       .filter((book) => book.bookNumber === 1 || book.bookNumber === null)
       .map(async (book) => {
         const cover = book.cover ? book.cover : null;
-        const series = book.series ? book.series.name : null;
+        const series = book.series ? book.series.name : '';
 
         // Get books by series and pass them into each response
         const querySeries = await queryBooksBySeries(series);
@@ -163,15 +168,50 @@ const buildBooksArray = async (allBooks) => {
     // Sort author alphabetically
     // If the first item comes first in the alphabet, move it up
     // Otherwise move it down
-    if (a.author > b.author) return 1;
-    if (a.author < b.author) return -1;
-
     // Then sort by title
-    if (a.title > b.title) return 1;
-    if (a.title < b.title) return -1;
+
+    const compareAuthor = a.author
+      .toLowerCase()
+      .localeCompare(b.author.toLowerCase());
+    const compareTitle = a.title
+      .toLowerCase()
+      .localeCompare(b.title.toLowerCase());
+
+    return compareAuthor || compareTitle;
   });
 
   return books;
 };
 
-export { querySingleBook, queryAllBooks, queryBooksBySeries, buildBooksArray };
+/**
+ * Sort an array by a value and then order by the title
+ *
+ * @param {Array} books
+ * @param {String} sortBy
+ * @returns {Array}
+ */
+const sortBooks = (books, sortBy) => {
+  // Sort sortBy value alphabetically
+  books.sort((a, b) => {
+    const compareSortByVal = a[sortBy]
+      .toLowerCase()
+      .localeCompare(b[sortBy].toLowerCase());
+    const compareTitle = a.title
+      .toLowerCase()
+      .localeCompare(b.title.toLowerCase());
+
+    // Check that the sortBy string isn't empty, and if so move the empty items to the end of the array
+    // See: https://stackoverflow.com/questions/63823339/sort-array-of-objects-alphabetically-and-move-the-empty-values-to-the-end for details on how this works
+    return !a[sortBy] - !b[sortBy] || compareSortByVal || compareTitle;
+  });
+
+  return books;
+};
+
+export {
+  querySingleBook,
+  queryAllBooks,
+  queryBooksBySeries,
+  buildBooksArray,
+  sortBooks,
+};
