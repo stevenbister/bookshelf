7a894bd1533f5bb9b71d6f53528ed5564bd4c5ee
Steven Bister <stevebister@gmail.com>
2022-05-25 15:27:31 +0100

    Feat: Add empty results state

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -3,55 +3,60 @@ import { kebabCase } from 'lodash';
 import Card from './Card/Card';
 import ExpandableCard from './Card/ExpandableCard';
 import ExpandableContent from './Card/ExpandableContent';
+import Empty from './Empty';
 
 const Grid = ({ array }) => {
   const [openSeries, setOpenSeries] = useState('');
 
-  return (
-    <ul className="grid" role="list">
-      {array.map(
-        ({ _id, title, slug, cover, series, readStatus, relatedBooks }) => {
-          return (
-            <React.Fragment key={_id}>
-              {series ? (
-                <>
-                  <ExpandableCard
-                    key={kebabCase(series) + '-' + _id}
-                    title={title}
-                    cover={cover}
-                    relatedBooks={relatedBooks}
-                    ariaControls={kebabCase(series)}
-                    onClick={() => {
-                      series === openSeries
-                        ? setOpenSeries('')
-                        : setOpenSeries(series);
-                    }}
-                  />
+  if (array.length > 0) {
+    return (
+      <ul className="grid" role="list">
+        {array.map(
+          ({ _id, title, slug, cover, series, readStatus, relatedBooks }) => {
+            return (
+              <React.Fragment key={_id}>
+                {series ? (
+                  <>
+                    <ExpandableCard
+                      key={kebabCase(series) + '-' + _id}
+                      title={title}
+                      cover={cover}
+                      relatedBooks={relatedBooks}
+                      ariaControls={kebabCase(series)}
+                      onClick={() => {
+                        series === openSeries
+                          ? setOpenSeries('')
+                          : setOpenSeries(series);
+                      }}
+                    />
 
-                  <ExpandableContent
-                    key={_id}
-                    id={kebabCase(series)}
-                    series={series}
-                    openSeries={openSeries}
-                    relatedBooks={relatedBooks}
+                    <ExpandableContent
+                      key={_id}
+                      id={kebabCase(series)}
+                      series={series}
+                      openSeries={openSeries}
+                      relatedBooks={relatedBooks}
+                    />
+                  </>
+                ) : (
+                  <Card
+                    key={slug.current + '-' + _id}
+                    title={title}
+                    slug={slug}
+                    cover={cover}
+                    readStatus={readStatus}
+                    headingLevel="h2"
                   />
-                </>
-              ) : (
-                <Card
-                  key={slug.current + '-' + _id}
-                  title={title}
-                  slug={slug}
-                  cover={cover}
-                  readStatus={readStatus}
-                  headingLevel="h2"
-                />
-              )}
-            </React.Fragment>
-          );
-        },
-      )}
-    </ul>
-  );
+                )}
+              </React.Fragment>
+            );
+          },
+        )}
+      </ul>
+    );
+  } else {
+    return <Empty />;
+  }
 };
 
 export default Grid;
