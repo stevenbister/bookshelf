507f182c27453396f8f08810ad7f342aec209d13
Steven Bister <stevebister@gmail.com>
2024-10-01 12:53:06 +0100

    add chunking to seed script

--- src/routes/api/seed/+server.ts
+++ src/routes/api/seed/+server.ts
@@ -1,19 +1,40 @@
 import { bookData } from '$lib/data/seed/book';
 import { statusData } from '$lib/data/seed/status';
+import type { Status as StatusType } from '$lib/db/schema/status';
 import { Author } from '$lib/queries/author';
 import { Book } from '$lib/queries/book';
 import { BookAuthor } from '$lib/queries/bookAuthor';
 import { BookSeries } from '$lib/queries/bookSeries';
+import { Cover } from '$lib/queries/cover';
 import { Series } from '$lib/queries/series';
 import { Status } from '$lib/queries/status';
+import { cfBindingNotFound } from '$lib/utils/cfBindingNotFound';
 import type { RequestHandler } from '@sveltejs/kit';
 import { drizzle } from 'drizzle-orm/d1';
-import type { Status as StatusType } from '$lib/db/schema/status';
-import { cfBindingNotFound } from '$lib/utils/cfBindingNotFound';
-import { Cover } from '$lib/queries/cover';
 
 export const GET: RequestHandler = async ({ platform }) => {
 	if (!platform?.env.DB) cfBindingNotFound();
+
+	const CHUNK_SIZE = 10;
+
+	function chunkArray<T>(array: T[], size: number): T[][] {
+		const chunks: T[][] = [];
+		for (let i = 0; i < array.length; i += size) {
+			chunks.push(array.slice(i, i + size));
+		}
+		return chunks;
+	}
+
+	async function processInChunks<T>(
+		items: T[],
+		chunkSize: number,
+		processFn: (chunk: T[]) => Promise<void>
+	) {
+		const chunks = chunkArray(items, chunkSize);
+		for (const chunk of chunks) {
+			await processFn(chunk);
+		}
+	}
 
 	const db = drizzle(platform.env.DB);
 
@@ -59,96 +80,110 @@ export const GET: RequestHandler = async ({ platform }) => {
 
 	console.log('Creating books...');
 
-	const seededBooks = await book.add(
-		await Promise.all(
-			bookData.map(async ({ title, blurb, status: sts, cover: cvr }) => {
-				const statusId = (await status.getIdByStatus(sts as StatusType['status']))[0].id;
-				const coverId = (await cover.getIdByUrl(cvr))?.[0].id;
+	await processInChunks(bookData, CHUNK_SIZE, async (chunk) => {
+		const seededBooks = await book.add(
+			await Promise.all(
+				chunk.map(async ({ title, blurb, status: sts, cover: cvr }) => {
+					const statusId = (await status.getIdByStatus(sts as StatusType['status']))[0].id;
+					const coverId = (await cover.getIdByUrl(cvr))?.[0].id;
 
-				return {
-					title,
-					blurb,
-					statusId,
-					coverId
-				};
-			})
-		)
-	);
-
-	console.log('Books created', seededBooks);
+					return {
+						title,
+						blurb,
+						statusId,
+						coverId
+					};
+				})
+			)
+		);
+		console.log('Books created', seededBooks);
+	});
 
 	console.log('Creating series...');
 
-	const seededSeries = await series.add(
-		bookData
-			.filter(({ series }) => !!series)
-			.map(({ series }) => ({
-				title: series!.title,
-				bookNumber: series?.number
-			}))
-	);
+	await processInChunks(
+		[...new Set(bookData.filter(({ series }) => !!series).map(({ series }) => series?.title))],
+		CHUNK_SIZE,
+		async (chunk) => {
+			const seededSeries = await series.add(
+				chunk.map((series) => ({
+					title: series!
+				}))
+			);
 
-	console.log('Series created', seededSeries);
+			console.log('Series created', seededSeries);
+		}
+	);
 
 	console.log('Creating authors...');
 
-	const seededAuthors = await author.add(
-		[
-			...new Set(bookData.flatMap((b) => b.author).map((a) => `${a.first_name} ${a.last_name}`))
-		].map((fullName) => {
-			const [first_name, last_name] = fullName.split(' ');
-			return {
-				first_name,
-				last_name
-			};
-		})
-	);
-
-	console.log('Authors created', seededAuthors);
-
-	console.log('Creating book authors...');
-
-	const bookAuthors = await Promise.all(
-		bookData.flatMap(async ({ title, author: a }) => {
-			const bookId = (await book.getIdByTitle(title))[0].id;
-
-			return Promise.all(
-				a.map(async ({ first_name, last_name }) => {
-					const authorId = (await author.getIdByName(first_name, last_name))[0].id;
-
+	await processInChunks(
+		[...new Set(bookData.flatMap((b) => b.author).map((a) => `${a.first_name} ${a.last_name}`))],
+		CHUNK_SIZE,
+		async (chunk) => {
+			const seededAuthors = await author.add(
+				chunk.map((fullName) => {
+					const [first_name, last_name] = fullName.split(' ');
 					return {
-						authorId,
-						bookId
+						first_name,
+						last_name
 					};
 				})
 			);
-		})
+
+			console.log('Authors created', seededAuthors);
+		}
 	);
 
-	const seededBookAuthors = await bookAuthor.add(bookAuthors.flat());
+	console.log('Creating book authors...');
 
-	console.log('Book authors created', seededBookAuthors);
+	await processInChunks(bookData, CHUNK_SIZE, async (chunk) => {
+		const bookAuthors = await Promise.all(
+			chunk.flatMap(async ({ title, author: a }) => {
+				const bookId = (await book.getIdByTitle(title))[0].id;
+
+				return Promise.all(
+					a.map(async ({ first_name, last_name }) => {
+						const authorId = (await author.getIdByName(first_name, last_name))[0].id;
+
+						return {
+							authorId,
+							bookId
+						};
+					})
+				);
+			})
+		);
+
+		const seededBookAuthors = await bookAuthor.add(bookAuthors.flat());
+
+		console.log('Book authors created', seededBookAuthors);
+	});
 
 	console.log('Creating book series...');
 
-	const seededBookSeries = await bookSeries.add(
-		await Promise.all(
-			bookData
-				.filter(({ series }) => series)
-				.map(async ({ title, series: srs }) => {
-					const bookId = (await book.getIdByTitle(title))[0].id;
-					const seriesId = (await series.getIdByTitle(srs!.title))[0].id;
+	await processInChunks(
+		bookData.filter(({ series }) => series),
+		CHUNK_SIZE,
+		async (chunk) => {
+			const seededBookSeries = await bookSeries.add(
+				await Promise.all(
+					chunk.map(async ({ title, series: srs }) => {
+						const bookId = (await book.getIdByTitle(title))[0].id;
+						const seriesId = (await series.getIdByTitle(srs!.title))[0].id;
 
-					return {
-						bookId,
-						seriesId,
-						bookNumber: srs?.number
-					};
-				})
-		)
+						return {
+							bookId,
+							seriesId,
+							bookNumber: srs?.number
+						};
+					})
+				)
+			);
+
+			console.log('Book series created', seededBookSeries);
+		}
 	);
-
-	console.log('Book series created', seededBookSeries);
 
 	console.log('Seeding finished 🌳');
 
