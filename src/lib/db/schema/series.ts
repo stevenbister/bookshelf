ccd3509af92e36dbf3bddc6559b1cfacb6697df4
Steven Bister <stevebister@gmail.com>
2024-10-25 16:54:15 +0100

    add series author column

--- src/lib/db/schema/series.ts
+++ src/lib/db/schema/series.ts
@@ -1,8 +1,10 @@
-import { sqliteTable, text, integer } from 'drizzle-orm/sqlite-core';
+import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { author } from './authors';
 
 export type Series = typeof series.$inferSelect;
 
 export const series = sqliteTable('series', {
 	id: integer('id').primaryKey({ autoIncrement: true }),
-	title: text('title').notNull()
+	title: text('title').notNull(),
+	seriesAuthorId: integer('series_author_id').references(() => author.id)
 });
