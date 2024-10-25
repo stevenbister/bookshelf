f7516aa41d7822eb515c4ca420a65e0c9b750918
Steven Bister <stevebister@gmail.com>
2024-10-25 17:02:18 +0100

    add initial function to search books

--- src/lib/queries/book.ts
+++ src/lib/queries/book.ts
@@ -1,11 +1,12 @@
-import { author } from '$lib/db/schema/authors';
+import { author, type Author } from '$lib/db/schema/authors';
 import { bookAuthor } from '$lib/db/schema/bookAuthor';
+import type { Book as BookType } from '$lib/db/schema/books';
 import { book } from '$lib/db/schema/books';
 import { bookSeries } from '$lib/db/schema/bookSeries';
 import { cover } from '$lib/db/schema/cover';
-import { series } from '$lib/db/schema/series';
-import { status } from '$lib/db/schema/status';
+import { series, type Series } from '$lib/db/schema/series';
 import type { DbClient } from '$lib/db/types';
+import { groupBy } from '$lib/utils/groupBy';
 import { eq } from 'drizzle-orm';
 import { TableCommon } from './table';
 
@@ -30,35 +31,68 @@ export class Book extends TableCommon<typeof book> {
 			.where(eq(this.schema.slug, slug));
 	}
 
-	async getBooksWithCover() {
-		return await this.db
-			.select({
-				title: this.schema.title,
-				slug: this.schema.slug,
-				cover: cover.url
-			})
-			.from(this.schema)
-			.leftJoin(cover, eq(this.schema.coverId, cover.id));
-	}
+	async search() {
+		type BookWithAuthors = Omit<BookType, 'blurb' | 'coverId' | 'statusId'> & {
+			authors: Author[];
+			cover: string | null;
+			series: Series | null;
+			seriesId?: Series['id'];
+			number?: number | null;
+		};
 
-	async getBookDetails() {
-		return await this.db
+		const results = await this.db
 			.select({
 				id: this.schema.id,
 				title: this.schema.title,
-				blurb: this.schema.blurb,
+				slug: this.schema.slug,
 				cover: cover.url,
 				author,
-				status: status.status,
 				series,
 				number: bookSeries.bookNumber
 			})
 			.from(this.schema)
-			.leftJoin(status, eq(this.schema.statusId, status.id))
 			.leftJoin(cover, eq(this.schema.coverId, cover.id))
 			.leftJoin(bookAuthor, eq(this.schema.id, bookAuthor.bookId))
 			.leftJoin(author, eq(bookAuthor.authorId, author.id))
 			.leftJoin(bookSeries, eq(this.schema.id, bookSeries.bookId))
 			.leftJoin(series, eq(bookSeries.seriesId, series.id));
+
+		const booksMap = new Map<number, BookWithAuthors>();
+		results.forEach((row) => {
+			let book = booksMap.get(row.id);
+
+			if (!book) {
+				const { id, title, slug, cover, number, series } = row;
+				book = { id, title, slug, cover, authors: [], number, series, seriesId: series?.id };
+				booksMap.set(id, book);
+			}
+
+			if (row.author) book.authors.push(row.author);
+		});
+
+		const booksWithAuthors = Array.from(booksMap.values());
+
+		const groupedBySeries: { [seriesId: number]: BookWithAuthors[] } = groupBy(
+			booksWithAuthors,
+			'seriesId'
+		);
+
+		for (const seriesId in groupedBySeries) {
+			const books = groupedBySeries[seriesId];
+			groupedBySeries[seriesId] = books.toSorted((a, b) => (a.number ?? 0) - (b.number ?? 0));
+		}
+
+		const sortedBooks = [...Object.values(groupedBySeries).flat()].toSorted((a, b) => {
+			if (a.series?.seriesAuthorId && b.series?.seriesAuthorId) {
+				return a.series.seriesAuthorId - b.series.seriesAuthorId;
+			}
+
+			const authorA = a.authors[0]?.first_name ?? '';
+			const authorB = b.authors[0]?.first_name ?? '';
+
+			return authorA.localeCompare(authorB);
+		});
+
+		return sortedBooks;
 	}
 }
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,12 @@
-import type { PageServerLoad } from './$types';
-import { Book } from '$lib/queries/book';
 import { Database } from '$lib/db/connection';
+import { Book } from '$lib/queries/book';
+import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async () => {
 	const db = Database.getInstance();
 	const book = new Book(db);
 
-	const books = await book.getBooksWithCover();
+	const books = await book.search();
 
 	return {
 		books
